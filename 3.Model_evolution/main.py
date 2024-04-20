5426de9de4923d1136f2277d2c80db6de3d6c5fb
LI Chenyu <153217294+chenyuli12345@users.noreply.github.com>
2024-04-20 09:59:09 +0800

    1

--- 3.Model_evolution/main.py
+++ 3.Model_evolution/main.py
@@ -14,99 +14,105 @@ import plot_figure2
 import plot_figure4_5
 import plot_figure6
 
-# Training settings
-parser = argparse.ArgumentParser(
-    description='Asymptotic stability study of SGD')
-parser.add_argument('--dataset', type=str, default='mnist',
-                    help="datset {'mnist', 'kmnist', 'emnist/mnist'}. default: 'mnist'")
-parser.add_argument('--activation-func', type=str, default='relu',
-                    help='activation function for hidden layers')
-parser.add_argument('--epochs', default=4, type=int, metavar='N',
-                    help='number of total epochs to run, should > 3')
-parser.add_argument('--batch-size', default=32, type=int, metavar='N',
-                    help='batch size for training')
-parser.add_argument('--optimizer', type=str, default='SGD',
-                    help='optimizer used for training')
-parser.add_argument('--lr', '--learning-rate', default=0.01, type=float,
-                    metavar='LR', help='initial learning rate', dest='lr')
-parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum for SGD')
-parser.add_argument('--beta-1', default=0.9, type=float, metavar='M',
-                    help='beta_1 in Adam')
-parser.add_argument('--beta-2', default=0.999, type=float, metavar='M',
-                    help='beta_2 in Adam')
-parser.add_argument('--weight-decay', default=0, type=float,
-                    metavar='W', help='weight decay (default: 0)',
-                    dest='weight_decay')
-parser.add_argument('--num-repeats', type=int, default=10,
-                    help='number of simulation repeats')
+# Training settings，设置训练参数
+
+#创建一个argparse.ArgumentParser对象，用于保存所有需要的信息，一边命令行参数解析到python数据类型
+parser = argparse.ArgumentParser(description='Asymptotic stability study of SGD') #提供一个描述信息
+
+#添加一些命令行参数，每个参数都有一个名称、类型、默认值和描述信息
+#数据集，默认为'mnist'
+parser.add_argument('--dataset', type=str, default='mnist', help="datset {'mnist', 'kmnist', 'emnist/mnist'}. default: 'mnist'") #--dataset参数，类型为字符串，默认值为mnist，描述信息为'datset {'mnist', 'kmnist', 'emnist/mnist'}. default: 'mnist'
+#激活函数，默认为'relu'
+parser.add_argument('--activation-func', type=str, default='relu', help='activation function for hidden layers') #--activation-func参数，类型为字符串，默认值为'relu'，描述信息为'activation function for hidden layers'
+#训练轮数，默认为4
+parser.add_argument('--epochs', default=4, type=int, metavar='N', help='number of total epochs to run, should > 3') #--epochs参数，类型为整数，默认值为4，描述信息为'number of total epochs to run, should > 3'
+#批次大小，默认为32
+parser.add_argument('--batch-size', default=32, type=int, metavar='N', help='batch size for training') #--batch-size参数，类型为整数，默认值为32，描述信息为'batch size for training'
+#优化器，默认为'SGD'
+parser.add_argument('--optimizer', type=str, default='SGD', help='optimizer used for training') #--optimizer参数，类型为字符串，默认值为'SGD'，描述信息为'optimizer used for training'
+#学习率，默认为0.01
+parser.add_argument('--lr', '--learning-rate', default=0.01, type=float, metavar='LR', help='initial learning rate', dest='lr') #--lr参数，类型为浮点数，默认值为0.01，描述信息为'initial learning rate'，dest='lr'表示将参数保存到args.lr中（可以通过此代码来获取这个值）
+#SGD算法的动量，默认为0.9
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum for SGD') #--momentum参数，类型为浮点数，默认值为0.9，描述信息为'momentum for SGD'
+#Adam算法的beta_1，默认为0.9
+parser.add_argument('--beta-1', default=0.9, type=float, metavar='M', help='beta_1 in Adam') #--beta-1参数，类型为浮点数，默认值为0.9，描述信息为'beta_1 in Adam'
+#Adam算法的beta_2，默认为0.999
+parser.add_argument('--beta-2', default=0.999, type=float, metavar='M', help='beta_2 in Adam') #--beta-2参数，类型为浮点数，默认值为0.999，描述信息为'beta_2 in Adam'
+#权重衰减，默认为0
+parser.add_argument('--weight-decay', default=0, type=float, metavar='W', help='weight decay (default: 0)', dest='weight_decay') #--weight-decay参数，类型为浮点数，默认值为0，描述信息为'weight decay (default: 0)'，dest='weight_decay'表示将参数保存到args.weight_decay中
+#重复次数，默认为10
+parser.add_argument('--num-repeats', type=int, default=10, help='number of simulation repeats') #--num-repeats参数，类型为整数，默认值为10，描述信息为'number of simulation repeats'
 
 
 def main():
-    start_time = time.time()
-    args = parser.parse_args()
-    args.log_epochs = np.arange(args.epochs)
+    start_time = time.time() #开始记录当前时间
+    args = parser.parse_args() #解析命令行参数，将参数保存到args中
+    args.log_epochs = np.arange(args.epochs) #创建一个数组，从0到args.epochs-1
+    #这是一个字符串，将所有参数组合在一起，用于后续的文件名，分别为“数据集_优化器_激活函数/epoch数量_10000倍学习率_批量大小_1000倍SGD算法动量_1000倍Adam算法参数beta1_10000000倍Adam算法参数beta2_1000000倍权重衰减”
     args.arguments = '{}_{}_{}/{}_{}_{}_{}_{}_{}_{}'.format(args.dataset, args.optimizer, args.activation_func, args.epochs, int(args.lr*10000), int(
         args.batch_size), int(args.momentum*1000), int(args.beta_1*1000), int(args.beta_2*10000000), int(args.weight_decay*1000000))
+    #三个新的字符串，用于后续的文件名，最后一个参数均为上面的字符串args.arguments
     args.save_weights_dir = 'rawdata/weights/{}'.format(args.arguments)
     args.save_losses_dir = 'rawdata/losses/{}'.format(args.arguments)
     args.save_scores_dir = 'rawdata/scores/{}'.format(args.arguments)
 
     # (x_train, y_train), (x_test, y_test) = utils.load_qmnist_data()
-    (x_train, y_train), (x_test, y_test) = utils.load_data(args.dataset)
-    args.input_shape = x_train.shape[1]
+    (x_train, y_train), (x_test, y_test) = utils.load_data(args.dataset) #加载数据集，x代表数据，y代表标签
+    args.input_shape = x_train.shape[1] #获取数据的第二维度
 
-    for num_repeat in range(args.num_repeats):
+    for num_repeat in range(args.num_repeats): #循环args.num_repeats次
         # break
-        print('num_repeat={}'.format(num_repeat))
-        args.save_weights_dir = 'rawdata/weights/{}/{}'.format(
-            args.arguments, num_repeat)
-        args.save_losses_dir = 'rawdata/losses/{}/{}'.format(
-            args.arguments, num_repeat)
-        args.save_scores_dir = 'rawdata/scores/{}/{}'.format(
-            args.arguments, num_repeat)
+        print('num_repeat={}'.format(num_repeat)) #打印当前循环次数
+        args.save_weights_dir = 'rawdata/weights/{}/{}'.format(args.arguments, num_repeat) #更新代表文件名的变量，加上当前循环次数
+        args.save_losses_dir = 'rawdata/losses/{}/{}'.format(args.arguments, num_repeat)
+        args.save_scores_dir = 'rawdata/scores/{}/{}'.format(args.arguments, num_repeat)
 
-        if args.activation_func == 'relu':
-            activation_func = tf.nn.relu
-            model = tf.keras.Sequential([
+
+        #设置神经网络模型
+        if args.activation_func == 'relu': #如果激活函数为'relu'
+            activation_func = tf.nn.relu #设置激活函数为relu
+            model = tf.keras.Sequential([ #创立一个顺序模型，这是最简单的神经网络模型，由多个网络层线性堆叠而成
                 tf.keras.layers.Dense(args.input_shape, activation=activation_func, name='layer_1',
                                       use_bias=False, input_shape=(args.input_shape,),
-                                      kernel_regularizer=keras.regularizers.l2(args.weight_decay),),
+                                      kernel_regularizer=keras.regularizers.l2(args.weight_decay),), #全连接层，输入维度为args.input_shape，激活函数为activation_func（relu），不使用偏置，输入形状为(args.input_shape,)，使用L2正则化，args.weight_decay为正则化系数
                 tf.keras.layers.Dense(10, activation=tf.nn.softmax,
-                                      kernel_regularizer=keras.regularizers.l2(args.weight_decay))
+                                      kernel_regularizer=keras.regularizers.l2(args.weight_decay)) #全连接层，输出维度为10，激活函数为softmax，使用L2正则化，args.weight_decay为正则化系数
             ])
-        elif args.activation_func == 'tanh':
-            activation_func = tf.nn.tanh
-            model = tf.keras.Sequential([
+        elif args.activation_func == 'tanh': #如果激活函数为'tanh'
+            activation_func = tf.nn.tanh #设置激活函数为tanh
+            model = tf.keras.Sequential([ #创立一个顺序模型，这是最简单的神经网络模型，由多个网络层线性堆叠而成
                 tf.keras.layers.Dense(args.input_shape, activation=activation_func, name='layer_1',
                                       kernel_initializer=tf.keras.initializers.RandomNormal(
                                           mean=(0)/(args.input_shape), stddev=1/(2*np.sqrt(args.input_shape))),
                                       use_bias=False, input_shape=(args.input_shape,),
-                                      kernel_regularizer=keras.regularizers.l2(args.weight_decay),),
+                                      kernel_regularizer=keras.regularizers.l2(args.weight_decay),), #全连接层，输入维度为args.input_shape，激活函数为activation_func（tanh），使用正态分布初始化，不使用偏置，输入形状为(args.input_shape,)，使用L2正则化，args.weight_decay为正则化系数
                 tf.keras.layers.Dense(10, activation=tf.nn.softmax,
                                       # kernel_initializer=tf.keras.initializers.RandomNormal(
                                       #     mean=(0)/(args.input_shape), stddev=1/(2*np.sqrt(args.input_shape))),
-                                      kernel_regularizer=keras.regularizers.l2(args.weight_decay))
+                                      kernel_regularizer=keras.regularizers.l2(args.weight_decay)) #全连接层，输出维度为10，激活函数为softmax，使用L2正则化，args.weight_decay为正则化系数
             ])
 
-        if args.optimizer == 'SGD':
-            optimizer = tf.keras.optimizers.SGD(
-                learning_rate=args.lr, momentum=args.momentum, nesterov=False, name='SGD')
-        elif args.optimizer == 'Adam':
-            optimizer = tf.keras.optimizers.Adam(
-                learning_rate=args.lr, beta_1=args.beta_1, beta_2=args.beta_2, epsilon=1e-07,
-                amsgrad=False, name='Adam')
-        metric_loss = SparseCategoricalCrossentropy(from_logits=False,
-                                                    name='sparse_categorical_crossentropy')
-        model.compile(optimizer=optimizer,
-                      loss='sparse_categorical_crossentropy',
-                      metrics=[metric_loss, 'accuracy'])
-        reporter = loggingreporter.LoggingReporter(
-            args, x_train, y_train, x_test, y_test)
+        #设置优化器
+        if args.optimizer == 'SGD': #如果优化器为'SGD'
+            optimizer = tf.keras.optimizers.SGD(learning_rate=args.lr, momentum=args.momentum, nesterov=False, name='SGD') #设置优化器为SGD，学习率为args.lr，动量为args.momentum，不使用Nesterov动量
+        elif args.optimizer == 'Adam': #如果优化器为'Adam'
+            optimizer = tf.keras.optimizers.Adam(learning_rate=args.lr, beta_1=args.beta_1, beta_2=args.beta_2, epsilon=1e-07, amsgrad=False, name='Adam') 
+            #设置优化器为Adam，学习率为args.lr，beta_1为args.beta_1，beta_2为args.beta_2，epsilon（一个非常小的数，用于防止除以0的错误）为1e-07，这里没有使用AMSGrad版本的Adam
+        
+        
+        metric_loss = SparseCategoricalCrossentropy(from_logits=False, name='sparse_categorical_crossentropy') #创建一个SparseCategoricalCrossentropy对象，这是一个损失函数，用于多分类问题，第一个参数表示模型的输出已经通过softmax函数转换为概率分布
+        #编译模型，指定优化器、损失函数和评估指标
+        model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=[metric_loss, 'accuracy']) #优化器为上一步设置的optimizer，损失函数为sparse_categorical_crossentropy，评估指标为sparse_categorical_crossentropy和accuracy
+        
+        #创建一个LoggingReporter对象，用于记录训练过程中的信息
+        reporter = loggingreporter.LoggingReporter(args, x_train, y_train, x_test, y_test)
         # model.fit(x_train, y_train, epochs=args.epochs,
         #           verbose=0, callbacks=[reporter, ], validation_split=0.2)
-        model.fit(x_train, y_train, epochs=args.epochs, batch_size=args.batch_size,
-                  verbose=0, callbacks=[reporter, ], validation_data=(x_test, y_test))
+        
+        #训练模型，前两个参数为训练数据，第三个参数为训练轮数，第四个参数为批次大小，verbose表示是否在标准输出流打印日志信息，callbacks表示在训练过程中调用的回调函数，validation_data表示验证数据
+        model.fit(x_train, y_train, epochs=args.epochs, batch_size=args.batch_size, verbose=0, callbacks=[reporter, ], validation_data=(x_test, y_test))
+
+        #清理TensorFlow的后端绘画，释放资源
         tf.keras.backend.clear_session()
 
     # Plot Fig.2 and Fig.3(a) in the paper.
--- 3.Model_evolution/utils.py
+++ 3.Model_evolution/utils.py
@@ -1,28 +1,36 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from scipy import io as spio
+from scipy import io as spio #导入scipy库的io模块，并将其重命名为spio。scipy是一个用于科学计算和技术计算的Python库，而io模块提供了一些输入/输出功能
 from tensorflow import keras
-import pickle
+import pickle #用来在文件之间存储和读取python对象，实现了基本的数据序列化（将对象信息保存到文件中）和反序列化（从文件中创建出原来的对象）
 
 
+
+#函数用于加载和预处理tensorflow指定的数据集，接收参数dataset_name，最后返回的是两个元组，第一个元组包含训练数据和训练标签，第二个元组包含测试数据和测试标签
 def load_data(dataset_name):
-    (x_train, y_train), (x_test, y_test) = tfds.as_numpy(tfds.load(
-        dataset_name, split=['train', 'test'], batch_size=-1, as_supervised=True,))
-    x_train = np.reshape(
-        x_train, [x_train.shape[0], -1]).astype('float32')/255.
+    #加载MNIST数据集，返回的是一个元组，元组中包含两个元组，第一个元组包含训练数据和训练标签，第二个元组包含测试数据和测试标签
+
+    #tfds.load表示从Tensorflow数据集库中加载指定的数据集，第一个参数为数据集名称；第二个参数为数据集的划分方式（这里分为train和test）；第三个参数为批处理大小（这里表示加载所有数据）；第四个参数表示是否以监督方式加载数据，这里表示数据将以输入/输出对的形式加载
+    #tfds.as_numpy表示将数据集转换为numpy数组
+    (x_train, y_train), (x_test, y_test) = tfds.as_numpy(tfds.load(dataset_name, split=['train', 'test'], batch_size=-1, as_supervised=True,))
+    
+    #将x_train和x_test的形状调整为二维，第一维度保持不变
+
+    #np.reshape表示将数据集的形状转换为指定的形状，第一个参数为数据集，第二个参数为新的形状；astype表示将数据集的数据类型转换为指定的数据类型
+    x_train = np.reshape(x_train, [x_train.shape[0], -1]).astype('float32')/255. #这里表示第一维度保持不变，第二维度将所有剩余元素平铺开来，还要除以255进行归一化
     x_test = np.reshape(x_test, [x_test.shape[0], -1]).astype('float32')/255.
 
     return (x_train, y_train), (x_test, y_test)
 
-
+#用于加载QMNIST（一种常用的手写数字识别数据集），返回的是两个元组，第一个元组包含训练数据和训练标签，第二个元组包含测试数据和测试标签
 def load_qmnist_data():
-    mnist = pickle.load(open('./data/qmnist.pkl', "rb"))
-    x_train, y_train = mnist['train_data'], mnist['train_labels']
-    x_test, y_test = mnist['test_data'], mnist['test_labels']
+    mnist = pickle.load(open('./data/qmnist.pkl', "rb")) #pickle.load表示从文件中加载数据，第一个参数为文件路径，第二个参数为读取模式（二进制模式），得到的是一个字典
+    x_train, y_train = mnist['train_data'], mnist['train_labels'] #从字典中取出训练数据和训练标签，x表示训练数据，y表示训练标签
+    x_test, y_test = mnist['test_data'], mnist['test_labels'] #从字典中取出测试数据和测试标签，x表示测试数据，y表示测试标签
 
-    x_train = np.reshape(
-        x_train, [x_train.shape[0], -1]).astype('float32')/255.
-    x_test = np.reshape(x_test, [x_test.shape[0], -1]).astype('float32')/255.
+    #np.reshape表示将数据集的形状转换为指定的形状，第一个参数为数据集，第二个参数为新的形状；astype表示将数据集的数据类型转换为指定的数据类型
+    x_train = np.reshape(x_train, [x_train.shape[0], -1]).astype('float32')/255. #这里表示第一维度保持不变，第二维度将所有剩余元素平铺开来，还要除以255进行归一化
+    x_test = np.reshape(x_test, [x_test.shape[0], -1]).astype('float32')/255. #这里表示第一维度保持不变，第二维度将所有剩余元素平铺开来，还要除以255进行归一化
 
-    return (x_train, y_train), (x_test, y_test)
+    return (x_train, y_train), (x_test, y_test) #返回两个元组，第一个元组包含训练数据和训练标签，第二个元组包含测试数据和测试标签
